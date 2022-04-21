6cb045e02db5c1464dc8862c277cde5450c9f4bb
Shilpa <shilpagaur29@gmail.com>
2022-04-21 21:55:56 +0530

    modify app java file

--- JenkinDemo/src/main/java/Maven/JenkinDemo/App.java
+++ JenkinDemo/src/main/java/Maven/JenkinDemo/App.java
@@ -1,13 +1,28 @@
 package Maven.JenkinDemo;
 
-/**
- * Hello world!
- *
- */
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+WebDriver driver;
+	
+	public void Login() {
+		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver_win32\\chromedriver.exe");
+        
+	 	driver = new ChromeDriver();
+	 	
+	 	driver.manage().window().maximize();
+	 	
+	 	driver.get("https://www.amazon.in/");
+	 	
+	 	driver.findElement(By.xpath("//a[@id='nav-link-accountList']")).click();
+	 	driver.findElement(By.xpath("//input[@id='ap_email']")).sendKeys("dummyem123@gmail.com");
+	 	driver.findElement(By.xpath("//span[@id='continue']")).click();
+	 	driver.findElement(By.xpath("//input[@id='ap_password']")).sendKeys("dm@12345");
+	 	driver.findElement(By.xpath("//input[@id='signInSubmit']")).submit();
+	}
+
 }
