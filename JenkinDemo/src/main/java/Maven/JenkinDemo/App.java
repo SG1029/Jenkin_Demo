d3e6ca060f60dcd738de11abf8c55b8c0828a71e
Shilpa <shilpagaur29@gmail.com>
2022-04-21 22:06:48 +0530

    modify app file

--- JenkinDemo/src/main/java/Maven/JenkinDemo/App.java
+++ JenkinDemo/src/main/java/Maven/JenkinDemo/App.java
@@ -3,12 +3,13 @@ package Maven.JenkinDemo;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.testng.annotations.Test;
 
 public class App 
 {
 WebDriver driver;
-	
+
+@Test	
 	public void Login() {
 		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver_win32\\chromedriver.exe");
         
@@ -23,6 +24,7 @@ WebDriver driver;
 	 	driver.findElement(By.xpath("//span[@id='continue']")).click();
 	 	driver.findElement(By.xpath("//input[@id='ap_password']")).sendKeys("dm@12345");
 	 	driver.findElement(By.xpath("//input[@id='signInSubmit']")).submit();
+	 	driver.close();
 	}
 
 }
